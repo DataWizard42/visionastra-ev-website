6434661938cb150bae53d06817cdef1089b88c91
Ayush Gayakwad <94968916+ayushgayakwad@users.noreply.github.com>
2024-08-17 17:34:16 +0530

    php script for contact.html

---
+++ php/submit_contact.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "host";
+$username = "username";
+$password = "password"; 
+$dbname = "database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$stmt = $conn->prepare("INSERT INTO contact_form (name, email, subject, message) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $name, $email, $subject, $message);
+
+if ($stmt->execute()) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+?>
