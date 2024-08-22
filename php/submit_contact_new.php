f9138dbdda6a2e2d6b28b27a10676b32ef7b1de9
Ayush Gayakwad <94968916+ayushgayakwad@users.noreply.github.com>
2024-08-22 16:13:58 +0530

    Update submit_contact_new.php

--- php/submit_contact_new.php
+++ php/submit_contact_new.php
@@ -13,7 +13,7 @@ if ($conn->connect_error) {
     exit();
 }
 
-$tableCreationQuery = "CREATE TABLE IF NOT EXISTS contact_form (
+$tableCreationQuery = "CREATE TABLE IF NOT EXISTS contact_form_new(
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
     email VARCHAR(50) NOT NULL,
